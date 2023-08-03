d11daed0ccaf3fc8a8ec43a3b7a58d171cd0a6a0
Froyston Mendonca <65276625+froyston@users.noreply.github.com>
2023-08-03 14:28:54 -0700

    Time: 17 ms, Memory: 55.9 MB - LeetHub

--- minimum-absolute-difference/minimum-absolute-difference.java
+++ minimum-absolute-difference/minimum-absolute-difference.java
@@ -19,19 +19,4 @@ class Solution {
         }
         return result;
     }
-    
-    public void sort(int[] nums) {
-        
-        for(int i = 1; i < nums.length; i++) {
-            int curr_idx = i;
-            
-            while(curr_idx > 0 && nums[curr_idx] < nums[curr_idx - 1]) {
-                
-                int temp = nums[curr_idx];
-                nums[curr_idx] = nums[curr_idx - 1];
-                nums[curr_idx - 1] = temp;
-                curr_idx -= 1;   
-            }
-        }
-    }
 }
