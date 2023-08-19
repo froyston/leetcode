4d780486003a81a152851fe2229dbbca8f4a0eb2
Froyston Mendonca <65276625+froyston@users.noreply.github.com>
2023-08-18 17:24:37 -0700

    Time: 1 ms (100.00%), Space: 44.9 MB (28.24%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
         
-        HashMap <Integer, Integer> seen = new HashMap<>();
+        int number = 0;
         
         for(int i = 0; i < nums.length; i++) {
-            if(!seen.containsKey(nums[i])) {
-                seen.put(nums[i] , 1);
-            }
-            else {
-                seen.remove(nums[i]);
-            }
+            number = number ^ nums[i];
         }
-        return seen.keySet().iterator().next();
+        return number;
     }
 }
