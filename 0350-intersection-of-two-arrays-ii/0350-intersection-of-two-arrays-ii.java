22259d88c50468618880f7d543d5d34f254e99bc
Froyston Mendonca <65276625+froyston@users.noreply.github.com>
2024-02-09 14:47:13 -0800

    Time: 3 ms (91.10%), Space: 43.5 MB (23.35%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,26 +1,21 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        int idx = 0;
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
         
-        int i = 0, j = 0, k = 0;
+        Map<Integer,Integer> commonElements = new HashMap<>();
+        for(int num : nums1) {
+            commonElements.put(num, commonElements.getOrDefault(num, 0) + 1);
+        }
         
-        while(i < nums1.length && j < nums2.length) {
-            
-            if(nums1[i] > nums2[j]) {
-                j++;
-            }
-            else if(nums1[i] < nums2[j]) {
-                i++;
-            }
-            else {
-                nums1[k] = nums1[i];
-                
-                i++;
-                j++;
-                k++;
+        for (int i = 0; i < nums2.length; i++) {
+            if(commonElements.containsKey(nums2[i]) && commonElements.get(nums2[i]) > 0) {
+                nums1[idx++] = nums2[i];
+                commonElements.put(nums2[i], commonElements.get(nums2[i]) - 1);
             }
         }
-        return Arrays.copyOfRange(nums1, 0 , k);
+        return Arrays.copyOfRange(nums1, 0, idx);
     }
 }
