39567113bcdc198e3ba14ac9a69bbb6919e8aeab
Froyston Mendonca <65276625+froyston@users.noreply.github.com>
2024-02-12 18:56:25 -0800

    Time: 4 ms (99.78%), Space: 46.8 MB (5.26%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -3,17 +3,18 @@ class Solution {
         int rows = grid.length;
         int columns = grid[0].length;
         
-        int up, down, left, right;
+        int up, left;
         int perimeter = 0;
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < columns; c++) {
                 if (grid[r][c] == 1) {
-                    up = (r > 0) ? grid[r - 1][c] : 0;
-                    left = (c > 0) ? grid[r][c - 1] : 0;
-                    down = (r + 1 == rows) ? 0 : grid[r + 1][c];
-                    right = (c + 1 == columns) ? 0 : grid[r][c + 1];
-                    
-                    perimeter = perimeter + (4 - up - down - left - right);
+                    perimeter += 4;
+                    if (r > 0) {
+                        perimeter = (grid[r - 1][c] == 1) ? perimeter -= 2 : perimeter;
+                    }
+                    if ( c > 0) {
+                        perimeter = (grid[r][c - 1] == 1)? perimeter -= 2 : perimeter;
+                    }
                 }
             }
         }
