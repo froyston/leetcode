703c6625824b590945d1e5a048e1845d911cf3ce
Froyston Mendonca <65276625+froyston@users.noreply.github.com>
2024-02-15 16:14:16 -0800

    Time: 0 ms (100.00%), Space: 41.8 MB (5.95%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,33 +1,32 @@
 class Solution {
     public String[] findWords(String[] words) {
-        String s1 = "qwertyuiop";
-        String s2 = "asdfghjkl";
-        String s3 = "zxcvbnm";
-        List<String> list = new ArrayList<>();
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+        List <String> result = new ArrayList<>();
+        
         for (String word : words) {
-            int[] row = new int[3];
+            int r1 = 0, r2 = 0, r3 = 0;
             for (char c : word.toLowerCase().toCharArray()) {
-                if (s1.indexOf(c) != -1) {
-                    row[0] = 1;
+                if (row1.indexOf(c) != -1) {
+                    r1 = 1;
                 }
-                else if (s2.indexOf(c) != -1) {
-                    row[1] = 1;
+                else if (row2.indexOf(c) != -1) {
+                    r2 = 1;
                 }
-                else if (s3.indexOf(c) != -1) {
-                    row[2] = 1;
+                else if (row3.indexOf(c) != -1) {
+                    r3 = 1;
                 }
             }
-            
-            if (row[0] + row[1] + row[2] == 1) {
-                list.add(word);
+            if (r1 + r2 + r3 == 1) {
+                result.add(word);
             }
         }
-        String[] arr = new String[list.size()];
-        int i=0;
-
-        for(String word : list)
-            arr[i++] = word;
-
-            return arr;
+        int i = 0;
+        String [] res = new String[result.size()];
+        for (String word : result) {
+            res[i++] = word;
+        }
+        return res;
     }
 }
