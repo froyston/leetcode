c3b59f48015662427d4caf584af8eed24070b060
Froyston Mendonca <65276625+froyston@users.noreply.github.com>
2024-02-16 18:02:54 -0800

    Time: 20 ms (93.11%), Space: 46.4 MB (9.90%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -1,9 +1,13 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
+        int len = candyType.length;
         Set<Integer> candyT = new HashSet<>();
         for (int candy : candyType) {
             candyT.add(candy);
+            if(candyT.size() == len / 2) {
+                return candyT.size();
+            }
         }
-        return Math.min(candyT.size(), candyType.length / 2);
+        return Math.min(candyT.size(), len / 2);
     }
 }
