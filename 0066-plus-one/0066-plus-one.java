11cd4e01b2d8d017a25792c81c1ac58494d2b4a9
Froyston Mendonca <65276625+froyston@users.noreply.github.com>
2024-02-22 15:53:29 -0800

    Time: 0 ms (100.00%), Space: 41.7 MB (69.45%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int carry = 0;
-        
-        for(int i = digits.length - 1; i >= 0 ; i--) {
+        for (int i = digits.length - 1 ; i >=0 ; i--) {
             if (digits[i] < 9) {
                 digits[i] += 1;
                 return digits;
-            }
-            else {
+            } else {
                 digits[i] = 0;
-                carry = Integer.max(carry+1 , 1);
             }
         }
-        int [] result = new int[digits.length + 1];
-        result[0] = 1;
-        System.arraycopy(digits, 0, result, 1, digits.length);
-        return result;
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        for (int i = 1; i < res.length; i++) {
+            res[i] = 0;
+        }
+        return res;
     }
 }
